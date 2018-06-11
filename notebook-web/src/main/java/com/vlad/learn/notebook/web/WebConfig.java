d82310d6290a3bb495f76906d652d8bb3760facb
Vladyslav_Sereda1 <vladyslav_sereda1@epam.com>
2018-06-11 12:32:49 +0300

    Hard-coded authentication

--- notebook-web/src/main/java/com/vlad/learn/notebook/web/WebConfig.java
+++ notebook-web/src/main/java/com/vlad/learn/notebook/web/WebConfig.java
@@ -2,6 +2,7 @@ package com.vlad.learn.notebook.web;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
 public class WebConfig {
@@ -10,6 +11,8 @@ public class WebConfig {
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new SpringSecurityDialect());
         return templateEngine;
     }
+
 }
