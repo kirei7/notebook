b9619458b47e217b41bf6738d582e544c4040898
Vladyslav_Sereda1 <vladyslav_sereda1@epam.com>
2018-06-19 15:23:31 +0300

    Tests now use mysql test db

--- notebook-app/src/test/java/com/vlad/learn/notebook/web/IntegrationTest.java
+++ notebook-app/src/test/java/com/vlad/learn/notebook/web/IntegrationTest.java
@@ -1,19 +1,39 @@
 package com.vlad.learn.notebook.web;
 
 import com.vlad.learn.notebook.NotebookApplication;
+import com.vlad.learn.notebook.core.entity.UserAccount;
+import com.vlad.learn.notebook.core.repository.UserAccountRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.PostConstruct;
 
+
+@ActiveProfiles("dev")
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = NotebookApplication.class)
 public class IntegrationTest {
 
+
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
+    private UserAccount userAccount;
+    private UserAccount userAccountGraph;
+
+    @PostConstruct
+    public void init() {
+        userAccount = userAccountRepository.findByEmail("mail@mail.com").get();
+        userAccountGraph = userAccountRepository.getByEmail("mail@mail.com").get();
+    }
+
     @Test
     public void contextStarts() {
-
+    int i = 2;
     }
 
 }
